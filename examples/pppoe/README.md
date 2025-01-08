6446423d220005d819e21af828d2c4a7d4ac365d
wizhannah <hannah@wiznet.io>
2025-01-08 13:21:35 +0900

    Add PPPoE example

---
+++ examples/pppoe/README.md
@@ -0,0 +1,140 @@
+# How to Test PPPoE Example
+
+
+
+## Step 1: Prepare software
+
+The following serial terminal programs are required for PPPoE example test, download and install from below links.
+
+- [**Tera Term**][link-tera_term]
+
+
+
+## Step 2: Prepare hardware
+
+If you are using W5100S-EVB-Pico, W5500-EVB-Pico, W55RP20-EVB-Pico, W5100S-EVB-Pico2 or W5500-EVB-Pico2, you can skip '1. Combine...'
+
+1. Combine WIZnet Ethernet HAT with Raspberry Pi Pico.
+
+2. Connect ethernet cable to WIZnet Ethernet HAT, W5100S-EVB-Pico, W5500-EVB-Pico, W55RP20-EVB-Pico, W5100S-EVB-Pico2 or W5500-EVB-Pico2 ethernet port.
+
+3. Connect Raspberry Pi Pico, W5100S-EVB-Pico or W5500-EVB-Pico to desktop or laptop using 5 pin micro USB cable. W55RP20-EVB-Pico, W5100S-EVB-Pico2 or W5500-EVB-Pico2 require a USB Type-C cable.
+
+
+
+## Step 3: Setup PPPoE Example
+
+To test the PPPoE example, minor settings shall be done in code.
+
+1. Setup SPI port and pin in 'w5x00_spi.h' in 'WIZnet-PICO-C/port/ioLibrary_Driver/' directory.
+
+Setup the SPI interface you use.
+- If you use the W5100S-EVB-Pico, W5500-EVB-Pico, W5100S-EVB-Pico2 or W5500-EVB-Pico2,
+
+```cpp
+/* SPI */
+#define SPI_PORT spi0
+
+#define PIN_SCK 18
+#define PIN_MOSI 19
+#define PIN_MISO 16
+#define PIN_CS 17
+#define PIN_RST 20
+```
+
+If you want to test with the PPPoE example using SPI DMA, uncomment USE_SPI_DMA.
+
+```cpp
+/* Use SPI DMA */
+//#define USE_SPI_DMA // if you want to use SPI DMA, uncomment.
+```
+- If you use the W55RP20-EVB-Pico,
+```cpp
+/* SPI */
+#define USE_SPI_PIO
+
+#define PIN_SCK 21
+#define PIN_MOSI 23
+#define PIN_MISO 22
+#define PIN_CS 20
+#define PIN_RST 25
+```
+
+2. Setup network configuration such as IP in 'w5x00_pppoe.c' which is the PPPoE example in 'WIZnet-PICO-C/examples/pppoe/' directory.
+
+Setup IP and other network settings to suit your network environment.
+
+```cpp
+/* Network */
+static wiz_NetInfo g_net_info =
+    {
+        .mac = {0x00, 0x08, 0xDC, 0x12, 0x34, 0x56}, // MAC address
+        .ip = {192, 168, 11, 2},                     // IP address
+        .sn = {255, 255, 255, 0},                    // Subnet Mask
+        .gw = {192, 168, 11, 1},                     // Gateway
+        .dns = {8, 8, 8, 8},                         // DNS server
+        .dhcp = NETINFO_STATIC                       // DHCP enable/disable
+};
+```
+
+3. Setup PPPoE configuration in 'w5x00_pppoe.c' in 'WIZnet-PICO-C/examples/pppoe/' directory.
+
+```cpp
+/* PPPoE */
+#define DATA_BUF_SIZE 2048
+
+/* PPPoE */
+uint8_t gDATABUF[DATA_BUF_SIZE];
+
+uint8_t pppoe_id[4] = "W5100S";
+uint8_t pppoe_id_len = 4;
+uint8_t pppoe_pw[4] = "WIZnet";
+uint8_t pppoe_pw_len = 4;
+uint8_t pppoe_ip[4] = {
+    0,
+};
+
+uint16_t pppoe_retry_count = 0;
+```
+
+
+
+## Step 4: Build
+
+1. After completing the PPPoE example configuration, click 'build' in the status bar at the bottom of Visual Studio Code or press the 'F7' button on the keyboard to build.
+
+2. When the build is completed, 'w5x00_pppoe.uf2' is generated in 'WIZnet-PICO-C/build/examples/pppoe/' directory.
+
+
+
+## Step 5: Upload and Run
+
+1. While pressing the BOOTSEL button of Raspberry Pi Pico, W5100S-EVB-Pico, W5500-EVB-Pico, W55RP20-EVB-Pico, W5100S-EVB-Pico2 or W5500-EVB-Pico2 power on the board, the USB mass storage 'RPI-RP2' is automatically mounted.
+
+![][link-raspberry_pi_pico_usb_mass_storage]
+
+2. Drag and drop 'w5x00_pppoe.uf2' onto the USB mass storage device 'RPI-RP2'.
+
+3. Connect to the serial COM port of Raspberry Pi Pico, W5100S-EVB-Pico, W5500-EVB-Pico, W55RP20-EVB-Pico, W5100S-EVB-Pico2 or W5500-EVB-Pico2 with Tera Term.
+
+![][link-connect_to_serial_com_port]
+
+4. Reset your board.
+
+5. Note that when using PPPOE for dialing, you must use a router with dialing function, enter the router management interface to enable the PPPOE function, obtain the ID and password, and write them into the code.
+
+6.  The main loop directly dials PPPOE to determine whether the dialing is successful. If successful, the obtained IP will be printed. If failed, the configured static IP will be printed.
+
+![][link-success_dialing]
+
+
+
+<!--
+Link
+-->
+
+[link-tera_term]: https://osdn.net/projects/ttssh2/releases/
+[link-hercules]: https://www.hw-group.com/software/hercules-setup-utility
+[link-raspberry_pi_pico_usb_mass_storage]: https://github.com/WIZnet-ioNIC/WIZnet-PICO-C/blob/main/static/images/pppoe/raspberry_pi_pico_usb_mass_storage.png
+[link-connect_to_serial_com_port]: https://github.com/WIZnet-ioNIC/WIZnet-PICO-C/blob/main/static/images/pppoe/connect_to_serial_com_port.png
+[link-success_dialing]: https://github.com/WIZnet-ioNIC/WIZnet-PICO-C/blob/main/static/images/pppoe/success_dialing.png
---
+++ examples/pppoe/w5x00_pppoe.c
@@ -0,0 +1,160 @@
+/**
+ * Copyright (c) 2021 WIZnet Co.,Ltd
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+/**
+ * ----------------------------------------------------------------------------------------------------
+ * Includes
+ * ----------------------------------------------------------------------------------------------------
+ */
+#include <stdio.h>
+
+#include "port_common.h"
+
+#include "wizchip_conf.h"
+#include "w5x00_spi.h"
+
+#include "PPPoE.h"
+
+/**
+ * ----------------------------------------------------------------------------------------------------
+ * Macros
+ * ----------------------------------------------------------------------------------------------------
+ */
+/* Clock */
+#define PLL_SYS_KHZ (133 * 1000)
+
+/* Buffer */
+#define ETHERNET_BUF_MAX_SIZE (1024 * 2)
+
+/* PPPoE */
+#define DATA_BUF_SIZE 2048
+
+/**
+ * ----------------------------------------------------------------------------------------------------
+ * Variables
+ * ----------------------------------------------------------------------------------------------------
+ */
+/* Network */
+static wiz_NetInfo g_net_info =
+    {
+        .mac = {0x00, 0x08, 0xDC, 0x12, 0x34, 0x56}, // MAC address
+        .ip = {192, 168, 11, 200},                     // IP address
+        .sn = {255, 255, 255, 0},                    // Subnet Mask
+        .gw = {192, 168, 11, 1},                     // Gateway
+        .dns = {8, 8, 8, 8},                         // DNS server
+        .dhcp = NETINFO_STATIC                       // DHCP enable/disable
+};
+
+/* PPPoE */
+uint8_t gDATABUF[DATA_BUF_SIZE];
+
+uint8_t pppoe_id[6] = "W5100S";
+uint8_t pppoe_id_len = 6;
+uint8_t pppoe_pw[6] = "WIZnet";
+uint8_t pppoe_pw_len = 6;
+uint8_t pppoe_ip[4] = {
+    0,
+};
+
+uint16_t pppoe_retry_count = 0;
+
+/**
+ * ----------------------------------------------------------------------------------------------------
+ * Functions
+ * ----------------------------------------------------------------------------------------------------
+ */
+/* Clock */
+static void set_clock_khz(void);
+
+/**
+ * ----------------------------------------------------------------------------------------------------
+ * Main
+ * ----------------------------------------------------------------------------------------------------
+ */
+int main()
+{
+    /* Initialize */
+    int32_t ret = 0;
+    uint8_t str[15];
+
+    set_clock_khz();
+
+    stdio_init_all();
+
+    wizchip_spi_initialize();
+    wizchip_cris_initialize();
+
+    wizchip_reset();
+    wizchip_initialize();
+    wizchip_check();
+
+    network_initialize(g_net_info);
+
+    printf("wiznet chip PPPOE example.\r\n");
+
+    while (1)
+    {
+        ret = ppp_start(gDATABUF); // ppp start function
+
+        if (ret == PPP_SUCCESS || pppoe_retry_count > PPP_MAX_RETRY_COUNT)
+        {
+            break; // PPPoE Connected or connect failed by over retry count
+        }
+            
+    }
+    if (ret == PPP_SUCCESS) // 1 : success
+    {
+
+        printf("\r\n<<<< PPPoE Success >>>>\r\n");
+        printf("Assigned IP address : %d.%d.%d.%d\r\n", pppoe_ip[0], pppoe_ip[1], pppoe_ip[2], pppoe_ip[3]);
+
+        printf("\r\n==================================================\r\n");
+        printf("    AFTER PPPoE, Net Configuration Information        \r\n");
+        printf("==================================================\r\n");
+
+        getSHAR(str);
+        printf("MAC address  : %x:%x:%x:%x:%x:%x\r\n", str[0], str[1], str[2], str[3], str[4], str[5]);
+        getSUBR(str);
+        printf("SUBNET MASK  : %d.%d.%d.%d\r\n", str[0], str[1], str[2], str[3]);
+        getGAR(str);
+        printf("G/W IP ADDRESS : %d.%d.%d.%d\r\n", str[0], str[1], str[2], str[3]);
+        getSIPR(str);
+        printf("SOURCE IP ADDRESS : %d.%d.%d.%d\r\n\r\n", str[0], str[1], str[2], str[3]);
+    }
+    else // failed
+    {
+        printf("\r\n<<<< PPPoE Failed >>>>\r\n");
+
+        /* Get network information */
+        print_network_information(g_net_info);
+    }
+
+    /* Infinite loop */
+    while (1)
+    {
+    }
+}
+
+/**
+ * ----------------------------------------------------------------------------------------------------
+ * Functions
+ * ----------------------------------------------------------------------------------------------------
+ */
+/* Clock */
+static void set_clock_khz(void)
+{
+    // set a system clock frequency in khz
+    set_sys_clock_khz(PLL_SYS_KHZ, true);
+
+    // configure the specified clock
+    clock_configure(
+        clk_peri,
+        0,                                                // No glitchless mux
+        CLOCKS_CLK_PERI_CTRL_AUXSRC_VALUE_CLKSRC_PLL_SYS, // System PLL on AUX mux
+        PLL_SYS_KHZ * 1000,                               // Input frequency
+        PLL_SYS_KHZ * 1000                                // Output (must be same as no divider)
+    );
+}
