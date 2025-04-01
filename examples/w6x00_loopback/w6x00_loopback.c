3b36d635128a1c563fba4d0af41967bb524a9e81
theoim <144100252+theoim@users.noreply.github.com>
2025-04-02 08:01:33 +0900

    change io-integration-library

---
+++ examples/w6x00_loopback/w6x00_loopback.c
@@ -0,0 +1,314 @@
+/**
+ * Copyright (c) 2021 WIZnet Co.,Ltd
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+/**
+ * ----------------------------------------------------------------------------------------------------
+ * Includes
+ * ----------------------------------------------------------------------------------------------------
+ */
+#include <stdio.h>
+
+#include "port_common.h"
+
+#include "wizchip_conf.h"
+#include "wizchip_spi.h"
+
+#include "loopback.h"
+
+#include "timer.h"
+
+/**
+ * ----------------------------------------------------------------------------------------------------
+ * Macros
+ * ----------------------------------------------------------------------------------------------------
+ */
+/* Clock */
+#define PLL_SYS_KHZ (133 * 1000)
+
+/* Buffer */
+#define ETHERNET_BUF_MAX_SIZE (1024 * 2)
+
+/* Socket */
+#define SOCKET_TCP_SERVER 0
+#define SOCKET_TCP_CLIENT 1
+#define SOCKET_UDP 2
+#define SOCKET_TCP_SERVER6 3
+#define SOCKET_TCP_CLIENT6 4
+#define SOCKET_UDP6 5
+#define SOCKET_TCP_SERVER_DUAL 6
+#define SOCKET_DHCP 7
+
+/* Port */
+#define PORT_TCP_SERVER 5000
+#define PORT_TCP_CLIENT 5001
+#define PORT_TCP_CLIENT_DEST    5002
+#define PORT_UDP 5003
+
+#define PORT_TCP_SERVER6 5004
+#define PORT_TCP_CLIENT6 5005
+#define PORT_TCP_CLIENT6_DEST 5006
+#define PORT_UDP6 5007
+
+#define PORT_TCP_SERVER_DUAL 5008
+
+#define IPV4
+// #define IPV6
+
+#ifdef IPV4
+#define TCP_SERVER
+// #define TCP_CLIENT
+// #define UDP
+#endif
+
+#ifdef IPV6
+#define TCP_SERVER6
+#define TCP_CLIENT6
+#define UDP6
+#endif
+
+#if defined IPV4 && defined IPV6
+#define TCP_SERVER_DUAL
+#endif
+
+#define RETRY_CNT   10000
+
+/**
+ * ----------------------------------------------------------------------------------------------------
+ * Variables
+ * ----------------------------------------------------------------------------------------------------
+ */
+/* Network */
+static wiz_NetInfo g_net_info =
+    {
+        .mac = {0x00, 0x08, 0xDC, 0x12, 0x34, 0x56}, // MAC address
+        .ip = {192, 168, 11, 22},                     // IP address
+        .sn = {255, 255, 255, 0},                    // Subnet Mask
+        .gw = {192, 168, 11, 1},                     // Gateway
+        .dns = {8, 8, 8, 8},                         // DNS server
+        .lla = {0xfe, 0x80, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00,
+                0x02, 0x08, 0xdc, 0xff,
+                0xfe, 0x57, 0x57, 0x25},             // Link Local Address
+        .gua = {0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00},             // Global Unicast Address
+        .sn6 = {0xff, 0xff, 0xff, 0xff,
+                0xff, 0xff, 0xff, 0xff,
+                0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00},             // IPv6 Prefix
+        .gw6 = {0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00},             // Gateway IPv6 Address
+        .dns6 = {0x20, 0x01, 0x48, 0x60,
+                0x48, 0x60, 0x00, 0x00,
+                0x00, 0x00, 0x00, 0x00,
+                0x00, 0x00, 0x88, 0x88},             // DNS6 server
+        .ipmode = NETINFO_STATIC_ALL
+};
+
+uint8_t tcp_client_destip[] = {
+    192, 168, 50, 103
+};
+
+uint8_t tcp_client_destip6[] = {
+    0x20, 0x01, 0x02, 0xb8,
+    0x00, 0x10, 0xff, 0xff,
+    0x71, 0x48, 0xcb, 0x27,
+    0x36, 0xb9, 0x99, 0x2e
+};
+
+uint16_t tcp_client_destport = PORT_TCP_CLIENT_DEST;
+
+uint16_t tcp_client_destport6 = PORT_TCP_CLIENT6_DEST;
+
+/* Loopback */
+static uint8_t g_tcp_server_buf[ETHERNET_BUF_MAX_SIZE] = {
+    0,
+};
+static uint8_t g_tcp_client_buf[ETHERNET_BUF_MAX_SIZE] = {
+    0,
+};
+static uint8_t g_udp_buf[ETHERNET_BUF_MAX_SIZE] = {
+    0,
+};
+static uint8_t g_tcp_server6_buf[ETHERNET_BUF_MAX_SIZE] = {
+    0,
+};
+static uint8_t g_tcp_client6_buf[ETHERNET_BUF_MAX_SIZE] = {
+    0,
+};
+static uint8_t g_udp6_buf[ETHERNET_BUF_MAX_SIZE] = {
+    0,
+};
+static uint8_t g_tcp_server_dual_buf[ETHERNET_BUF_MAX_SIZE] = {
+    0,
+};
+
+/* Timer */
+static volatile uint16_t g_msec_cnt = 0;
+
+/**
+ * ----------------------------------------------------------------------------------------------------
+ * Functions
+ * ----------------------------------------------------------------------------------------------------
+ */
+/* Clock */
+static void set_clock_khz(void);
+
+/* Timer */
+static void repeating_timer_callback(void);
+
+/**
+ * ----------------------------------------------------------------------------------------------------
+ * Main
+ * ----------------------------------------------------------------------------------------------------
+ */
+int main()
+{
+    /* Initialize */
+    int retval = 0;
+    uint8_t dhcp_retry = 0;
+    uint8_t dns_retry = 0;
+
+    set_clock_khz();
+
+    stdio_init_all();
+
+    sleep_ms(1000 * 3);
+
+    printf("==========================================================\n");
+    printf("Compiled @ %s, %s\n", __DATE__, __TIME__);
+    printf("==========================================================\n");
+
+    wizchip_spi_initialize();
+    printf("spi init!!\r\n");
+    wizchip_cris_initialize();
+    printf("cris init!!\r\n");
+    
+    wizchip_reset();
+    printf("chip RST!!\r\n");
+    wizchip_initialize();
+    printf("chip init!!\r\n");
+    wizchip_check();
+
+    // wizchip_1ms_timer_initialize(repeating_timer_callback);
+
+    network_initialize(g_net_info);
+    printf("network info init!!\r\n");
+
+    /* Get network information */
+    print_network_information(g_net_info);
+    set_loopback_mode_W6x00(AS_IPV4);
+    /* Infinite loop */
+    while (1)
+    {
+#ifdef TCP_SERVER
+            /* TCP server loopback test */
+            if ((retval = loopback_tcps(SOCKET_TCP_SERVER, g_tcp_server_buf, PORT_TCP_SERVER)) < 0)
+            {
+                printf(" loopback_tcps error : %d\n", retval);
+
+                while (1)
+                    ;
+            }
+#endif
+#ifdef TCP_CLIENT
+            /* TCP client loopback test */
+            static uint32_t tcp_client_cnt = 0;
+            if ((retval = loopback_tcpc(SOCKET_TCP_CLIENT, g_tcp_client_buf, tcp_client_destip, tcp_client_destport)) < 0)
+            {
+                printf(" loopback_tcpc error : %d\n", retval);
+
+                while (1)
+                    ;
+            }
+#endif
+#ifdef UDP
+            /* UDP loopback test */
+            if ((retval = loopback_udps(SOCKET_UDP, g_udp_buf, PORT_UDP)) < 0)
+            {
+                printf(" loopback_udps error : %d\n", retval);
+
+                while (1)
+                    ;
+            }
+#endif
+#ifdef TCP_SERVER6
+            /* TCP server loopback test */
+            if ((retval = loopback_tcps(SOCKET_TCP_SERVER6, g_tcp_server6_buf, PORT_TCP_SERVER6)) < 0)
+            {
+                printf(" loopback_tcps IPv6 error : %d\n", retval);
+
+                while (1)
+                    ;
+            }
+#endif
+#ifdef TCP_CLIENT6
+            /* TCP client loopback test */
+            if ((retval = loopback_tcpc(SOCKET_TCP_CLIENT6, g_tcp_client6_buf, tcp_client_destip6, tcp_client_destport6)) < 0)
+            {
+                printf(" loopback_tcpc IPv6 error : %d\n", retval);
+
+                while (1)
+                    ;
+            }
+#endif
+#ifdef UDP6
+            /* UDP loopback test */
+            if ((retval = loopback_udps(SOCKET_UDP6, g_udp6_buf, PORT_UDP6)) < 0)
+            {
+                printf(" loopback_udps IPv6 error : %d\n", retval);
+
+                while (1)
+                    ;
+            }
+#endif
+#ifdef TCP_SERVER_DUAL
+            /* TCP server dual loopback test */
+            if ((retval = loopback_tcps(SOCKET_TCP_SERVER_DUAL, g_tcp_server_dual_buf, PORT_TCP_SERVER_DUAL, AS_IPDUAL)) < 0)
+            {
+                printf(" loopback_tcps IPv6 error : %d\n", retval);
+
+                while (1)
+                    ;
+            }
+#endif
+    }
+}
+
+/**
+ * ----------------------------------------------------------------------------------------------------
+ * Functions
+ * ----------------------------------------------------------------------------------------------------
+ */
+/* Clock */
+static void set_clock_khz(void)
+{
+    // set a system clock frequency in khz
+    set_sys_clock_khz(PLL_SYS_KHZ, true);
+
+    // configure the specified clock
+    clock_configure(
+        clk_peri,
+        0,                                                // No glitchless mux
+        CLOCKS_CLK_PERI_CTRL_AUXSRC_VALUE_CLKSRC_PLL_SYS, // System PLL on AUX mux
+        PLL_SYS_KHZ * 1000,                               // Input frequency
+        PLL_SYS_KHZ * 1000                                // Output (must be same as no divider)
+    );
+}
+
+/* Timer */
+static void repeating_timer_callback(void)
+{
+    g_msec_cnt++;
+
+    if (g_msec_cnt >= 1000 - 1)
+    {
+        g_msec_cnt = 0;
+    }
+}
--- port/ioLibrary_Driver/src/wizchip_gpio_irq.c
+++ port/ioLibrary_Driver/src/wizchip_gpio_irq.c
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2021 WIZnet Co.,Ltd
+ * Copyright (c) 2022 WIZnet Co.,Ltd
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -12,40 +12,48 @@
 #include <stdio.h>
 
 #include "pico/stdlib.h"
+#include "hardware/gpio.h"
 
-#include "timer.h"
+#include "wizchip_conf.h"
+#include "socket.h"
+#include "wizchip_gpio_irq.h"
 
 /**
  * ----------------------------------------------------------------------------------------------------
  * Variables
  * ----------------------------------------------------------------------------------------------------
  */
-/* Timer */
-static struct repeating_timer g_timer;
-void (*callback_ptr)(void);
+static void (*callback_ptr)(void);
 
 /**
  * ----------------------------------------------------------------------------------------------------
  * Functions
  * ----------------------------------------------------------------------------------------------------
  */
-/* Timer */
-void wizchip_1ms_timer_initialize(void (*callback)(void))
+/* GPIO */
+void wizchip_gpio_interrupt_initialize(uint8_t socket, void (*callback)(void))
 {
+    uint16_t reg_val;
+    int ret_val;
+
+    reg_val = (SIK_CONNECTED | SIK_DISCONNECTED | SIK_RECEIVED | SIK_TIMEOUT); // except SendOK
+    ret_val = ctlsocket(socket, CS_SET_INTMASK, (void *)&reg_val);
+
+#if (_WIZCHIP_ == W5100S)
+    reg_val = (1 << socket);
+#elif (_WIZCHIP_ == W5500)
+    reg_val = ((1 << socket) << 8);
+#endif
+    ret_val = ctlwizchip(CW_SET_INTRMASK, (void *)&reg_val);
+
     callback_ptr = callback;
-    add_repeating_timer_us(-1000, wizchip_1ms_timer_callback, NULL, &g_timer);
+    gpio_set_irq_enabled_with_callback(PIN_INT, GPIO_IRQ_EDGE_FALL, true, &wizchip_gpio_interrupt_callback);
 }
 
-bool wizchip_1ms_timer_callback(struct repeating_timer *t)
+static void wizchip_gpio_interrupt_callback(uint gpio, uint32_t events)
 {
     if (callback_ptr != NULL)
     {
         callback_ptr();
     }
-}
-
-/* Delay */
-void wizchip_delay_ms(uint32_t ms)
-{
-    sleep_ms(ms);
 }
